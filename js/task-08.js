05a7f7f09326cfddaeedce5d9f54f24ad8532c58
Elena Pastushenko <heleora_net@ukr.net>
2022-10-08 21:09:37 +0300

    Task 8 is done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const loginFormRef = document.querySelector(".login-form");
+
+loginFormRef.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const { elements: { email, password } } = event.currentTarget;
+    
+      if (email.value === "" || password.value === "") {
+        alert("Please fill all the fields in!");
+      };
+
+      const userEmailAndPassword = {
+        email: email.value,
+        password: password.value,
+      };
+
+      console.log(userEmailAndPassword);
+      event.currentTarget.reset();
+})
+
 
