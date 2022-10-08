646f8dcfe87253496da11b74f2cdac9ac907441b
Elena Pastushenko <heleora_net@ukr.net>
2022-10-08 18:44:31 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,16 +1,16 @@
 const validationInput = document.querySelector("#validation-input");
 
 validationInput.addEventListener("blur", (event) => {
-   const inputRef = event.currentTarget;
+    const inputRef = event.currentTarget;
 
    inputRef.classList.add("invalid");
 
-   if (inputRef.value.length == inputRef.attributes[2].value) {
-      // console.log(inputRef.value.length);
+   if (inputRef.value.length == inputRef.getAttribute("data-length")) {
       inputRef.classList.remove("invalid")
       inputRef.classList.add("valid");
-       };
-  
+      };
 });
 
-// ???? Как получить ссылку на атрибут с именем, в котором есть "-" ??!!!!
+
+// inputRef.value.length == inputRef.attributes[2].value
+
