3db55ac9a33f3164dba36c69273af439bce62225
Elena Pastushenko <heleora_net@ukr.net>
2022-10-09 00:07:40 +0300

    Task 10 is done

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,35 @@
+const boxesRef = document.querySelector("#boxes");
+const btnCreateBoxesRef = document.querySelector("button[data-create]");
+const btnDestroyBoxesRef = document.querySelector("button[data-destroy]");
+const inputAmountRef = document.querySelector("#controls input");
+
+btnCreateBoxesRef.addEventListener("click", () => {
+    createBoxes(inputAmountRef.value);
+});
+
+btnDestroyBoxesRef.addEventListener("click", () => {
+    boxesRef.textContent = "";
+    inputAmountRef.value = "";
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function createBoxes(amount) {
+   let blocks = [];
+   
+   for (let i = 0; i < amount; i += 1) {
+    
+    const box = document.createElement("div");
+    box.style.width = `${30 + i*10}px`;
+    box.style.height = `${30 + i*10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+   
+    blocks.push(box);
+
+    boxesRef.append(...blocks);
+
+  };
+  };
+
