eade7b4a407aaedd86aeabc07e0de89edf80b5d3
Elena Pastushenko <heleora_net@ukr.net>
2022-10-09 12:24:51 +0300

    Update function getRandomHexColor()

--- js/task-10.js
+++ js/task-10.js
@@ -13,7 +13,7 @@ btnDestroyBoxesRef.addEventListener("click", () => {
 });
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
 
 function createBoxes(amount) {
